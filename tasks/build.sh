c3bfa2d5d3ec7379f68967d12db69965c8083ace
dmi3y <lapshukov@gmail.com>
2016-05-15 20:27:37 -0700

    copy assets for build

--- tasks/build.sh
+++ tasks/build.sh
@@ -5,6 +5,7 @@ npm run copy-dependencies & \
 npm run compile-templates && npm run build-index \
 npm run compile-scripts & \
 npm run copy-app-config & \
+npm run unlink-assets && npm run copy-assets & \
 npm run compile-styles &
 
 wait
