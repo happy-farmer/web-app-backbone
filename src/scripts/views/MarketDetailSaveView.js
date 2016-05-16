44bd47232461257f93b97a484eb688eaaeaade75
dmi3y <lapshukov@gmail.com>
2016-05-15 19:57:34 -0700

    save view

---
+++ src/scripts/views/MarketDetailSaveView.js
@@ -0,0 +1,32 @@
+/*
+ * @module views/MarketDetailSaveView
+ */
+
+ /* global define */
+
+define((require) => {
+  'use strict'
+  var template = require('mixins/templates').marketDetailSave
+  var Mt = require('marionette')
+  var $ = require('mixins/jqueryPlus')
+  var _ = require('underscore')
+
+  return Mt.ItemView.extend({
+    template,
+    events: {
+      'submit form': 'send'
+    },
+    initialize: function (options) {
+      this.options = _.extend(this._defaults, options)
+    },
+    send (ev) {
+      ev.preventDefault()
+      var form = $(ev.currentTarget)
+      var data = form.serializeObject()
+      this.model.save(data, this.options.save)
+    },
+    _defaults: {
+      save: {}
+    }
+  })
+})
